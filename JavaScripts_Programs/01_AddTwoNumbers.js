7241bb36a8c230db9837bfd1ecaf8045e76b0cf3
abhivaadyasharma <abhivaadya.sharma@gmail.com>
2026-01-20 17:55:23 +0530

    Modified program- 01_AddTwoNumbers

--- JavaScripts_Programs/01_AddTwoNumbers.js
+++ JavaScripts_Programs/01_AddTwoNumbers.js
@@ -1,6 +1,11 @@
 //Author: Abhivaadya Sharma
 
-let num1 = 5;
-let num2 = 7;
+const ps = require("prompt-sync");
+const prompt = ps();
+
+let num1 = prompt("Enter number-1: ");
+num1=Number.parseInt(num1)
+let num2 = prompt("Enter number-2: ");
+num2=Number.parseInt(num2)
 let sum = num1 + num2;
 console.log("Sum: " + sum);
