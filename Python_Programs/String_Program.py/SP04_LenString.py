84e03777007134c2b770b01eba77be172f11d9fc
abhivaadyasharma <abhivaadya.sharma@gmail.com>
2026-01-21 18:26:09 +0530

    Added Python program to find the length of a string.

---
+++ Python_Programs/String_Program.py/SP04_LenString.py
@@ -0,0 +1,11 @@
+#Author:Abhivaadya Sharma
+
+#Asking for string
+String=input("Enter the string: ")
+
+#To find Length of string 
+Len_of_String=len(String)
+
+#Giving output
+print(f"Length of the string entered is {Len_of_String}")
+
