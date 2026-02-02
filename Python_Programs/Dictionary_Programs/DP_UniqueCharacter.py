8a0a4663e822f9cec0485250c9e39b601891bb69
abhivaadyasharma <abhivaadya.sharma@gmail.com>
2026-02-02 20:05:37 +0530

    Python Program Extract Unique values dictionary values

---
+++ Python_Programs/Dictionary_Programs/DP_UniqueCharacter.py
@@ -0,0 +1,13 @@
+# Author: Abhivaadya Sharma
+
+marks = {
+    "Aarzu": 97,
+    "Abhivaadya": 99.98,
+    "Aadil": 78,
+    "Manan": 98.88,
+    "Mehar": 88
+}
+
+unique_values = set(marks.values())
+
+print(unique_values)
